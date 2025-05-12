5644d873acec9546eddd24a6236120d461bb6cef
coopergh3 <coopergh3@gmail.com>
2025-05-12 00:46:52 -0300

    Sales model added with indexes by (product, date) and (region, date)

--- app/models.py
+++ app/models.py
@@ -1,7 +1,25 @@
+from . import db
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Index
 from werkzeug.security import generate_password_hash, check_password_hash
 
-db= SQLAlchemy()
+class Sales(db.Model):
+    __tablename__ = 'sales'
+    id= db.Column(db.Integer, primary_key=True)
+    product_name= db.Column(db.String(50), nullable=False)
+    quantity= db.Column(db.Integer, nullable=False)
+    price= db.Column(db.Float, nullable=False)
+    date = db.Column(db.DateTime, default=db.func.current_timestamp())
+    created_at= db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at= db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+    @property
+    def revenue(self):
+        return self.quantity * self.price
+    
+Index('idx_product_date', Sales.product, Sales.date)
+Index('idx_region_date', Sales.region, Sales.date)
+
 
 class User(db.Model):
     __tablename__ = 'users'
