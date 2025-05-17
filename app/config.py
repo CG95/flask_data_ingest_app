ab911eaf5a7637aed916020fb8370358be424aed
coopergh3 <coopergh3@gmail.com>
2025-05-16 23:11:59 -0300

    enforce LF for unix shell scripts

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CACHE_TYPE = 'RedisCache'
-    CACHE_REDIS_HOST = 'localhost'
+    CACHE_REDIS_HOST = os.getenv("CACHE_REDIS_HOST", "localhost")
     CACHE_REDIS_PORT = 6379
     CACHE_REDIS_DB = 0
     CACHE_DEFAULT_TIMEOUT = 300
